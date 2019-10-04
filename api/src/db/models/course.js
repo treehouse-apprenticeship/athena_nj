5ec1b492fda2b882ab9b9c3bdb1566955b65ea3f
Lujanhi <51241524+Lujanhi@users.noreply.github.com>
2019-10-04 10:48:02 -0400

    fixed  spelling in course model

--- api/src/db/models/course.js
+++ api/src/db/models/course.js
@@ -25,7 +25,7 @@ const courseSchema = Schema({
     type: String,
     required: [true, 'Please provide a value for "Description"'],
   },
-  estimatedTme: String,
+  estimatedTime: String,
   materialsNeeded: String,
 });
 
